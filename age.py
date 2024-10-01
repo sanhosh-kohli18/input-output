60d067a93d10a91ea8191279530e905dd42c80d8
SANTHOSH5A0 <santoshsantosh85118@gmail.com>
2024-10-01 10:48:28 +0530

    Create age.py

---
+++ age.py
@@ -0,0 +1,11 @@
+Input is an integer that corresponds to the age of Dhoni.
+Output Format: 
+Display the age.
+Refer sample Input and Output for formatting specifications.
+[All the text in bold corresponds to the Input]
+Sample Input and Output:
+.
+.
+Enter the age:
+35
+Age of Dhoni is 35
